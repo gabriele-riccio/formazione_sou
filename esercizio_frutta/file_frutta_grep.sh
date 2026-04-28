a12efc8b5360e0cd24203fa35593e4a8dc460efb
Gabriele <ricciogabriele0@gmail.com>
2026-04-28 12:21:10 +0200

    Aggiunti script AWK, grep e sed con CSV di test e README

---
+++ esercizio_frutta/file_frutta_grep.sh
@@ -0,0 +1,9 @@
+
+##Grep con cut
+
+grep "banana" file_frutta.csv | cut -d',' -f3
+
+#mi restituisce
+strawberry
+orange
+
---
+++ esercizio_frutta/file_frutta_sed.sh
@@ -0,0 +1,7 @@
+## Sed
+sed -n '/banana s/^[^,]*,[^,]*,//p' file_frutta.csv
+
+#mi restituisce
+strawberry
+orange
+
