aaa15a021cb95951c37442bbfd359f7947b01640
gabriele-riccio <gabriele.riccio@sourcesense.com>
2026-04-29 15:09:04 +0200

    Update Vagrant_file.sh
    Added port forwarding for HTTP and HTTPS on target VM.

--- esercizio_portscanner/Vagrant_file.sh
+++ esercizio_portscanner/Vagrant_file.sh
@@ -1,20 +1,24 @@
 Vagrant.configure("2") do |config|
-  # Ho usato un'immagine Linux standard (es Ubuntu)
   config.vm.box = "bento/ubuntu-20.04"
 
-  # VM 1: La prima virtual machine è lo  Scanner
+  # VM 1: Lo Scanner
   config.vm.define "scanner" do |scanner|
     scanner.vm.hostname = "scanner"
     scanner.vm.network "private_network", ip: "192.168.50.10"
   end
 
-  # VM 2: La seconda virtualmachine è il target o bersaglio
+  # VM 2: Il Bersaglio (Target)
   config.vm.define "target" do |target|
     target.vm.hostname = "target"
     target.vm.network "private_network", ip: "192.168.50.11"
+
+    # Port forwarding
+    target.vm.network "forwarded_port", guest: 80,  host: 8080
+    target.vm.network "forwarded_port", guest: 443, host: 8443
   end
+
   config.vm.provider "virtualbox" do |vb|
-    # Qui ho personalizzato la VM, ad esempio ho assegnato  1GB di RAM
     vb.memory = "1024"
+  end
 
 end
