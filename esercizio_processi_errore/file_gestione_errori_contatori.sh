e2f8894c8333555ee59259826d143d8d0cc4ce1f
Gabriele <ricciogabriele0@gmail.com>
2026-05-04 09:36:23 +0200

    Parte con i contatori

---
+++ esercizio_processi_errore/file_gestione_errori_contatori.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#================================================================================
+#Nome file= file_gestione_processi.sh
+#Descrizione esercizio= Gestione dei processi,cicli,errori e contatori in Bash
+#================================================================================
+#in questa parte voglio procedere all'inserimento dei contatori, uno che conta i successi e uno che conta gli errori e un report finale.
+
+
+
+#1)Prima cosa definisco delle variabili:le metto in maiuscolo proprio perchè devono essere costanti.
+
+NUM_PROCESSI=5
+
+#è ilnumero di volte che il ciclo for deve girare, per il momento ne lascio 5 poi vedrò se aumentare.
+
+#2)Definisco i contatori uno per i successi e uno per gli errori.
+
+count_success=0
+count_error=0
+#partono da zero e vengono incrementati man mano.
+
+#Ciclo for, dove effettuo una simulazione del processo, per ogni iterazione creo un file, vedendo ulteriormente se c'è l'errore.
+#Praticamente uguale a prima ma aggiungo l'incremento del contatore successo e errore se fallisce.
+for((i=1; i<=NUM_PROCESSI; i++))do
+        echo "Processo $i  in esecuzione..."
+        # Simulazione processo: creiamo un file per ogni iterazione
+        if echo "dati processo $i" > "processo_$i.txt"; then
+                # $? == 0 : comando riuscito
+                echo "Processo $i: successo!"
+                ((count_success++))
+        else
+                # $? != 0 : comando fallito
+                echo "Processo $i: errore!"
+                ((count_error++))
+        fi
+
+done
+#N.B Anche in questo caso ci saranno solo successi.
+
+#Faccio un report finale con numero successi e numero errori.
+# --- Report ---
+echo "-------------------------------------"
+echo "Totale  : $NUM_PROCESSI"
+echo "Successi: $count_success"
+echo "Errori  : $count_error"
+echo "-------------------------------------"
+
