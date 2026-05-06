93c7d465003a448e83fcae38cf2da8ac124557af
Gabriele <ricciogabriele0@gmail.com>
2026-05-06 15:29:08 +0200

    feat: aggiunto esercizio

---
+++ esercitazione7/shell_linux_pdf/file_esercizio6.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#--Esercizio 5--Creare un file con una lista di 8 file in /usr/sbin ordinati per momento dell'ultima modifica.
+
+#Creare un file con una lista di 8 comandi di  usr/sbin ordinati per momento dell'ultima modifica.
+#Uso ls con -t per ordinare nel tempo come ultima modifica.
+
+ls -t /usr/sbin
+
+#output:
+         ntfsclone                 ...         ...
+         mkntfs                  ...         ...
+         mount.ntfs                             ...         ...
+         mount.ntfs-3g                          ...         ...
+         ntfslabel
+         mount.lowntfs-3g
+         ntfscp
+         mkfs.ntfs
+         ...
+         ...
+         ...
+
+#Lista di file al suo interno in ordine temporale,secondo l'ultima modifica.
+
+#Per creare il file con gli ultimi file di usr/sbin uso la pipe | utilizzando head -8 per salvare le prime 8 righe e con la funzione freccia > li salvo in uni nuovo file che crea in automatico, lo chiamo top8_modifica_usrsbin.txt
+
+#Eseguo:
+
+
+ls -t /usr/sbin |  head -8 > top8_modifica_usrsbin.txt
+
+#output: nulla, ha salvato i file in top8_modifica_usrsbin.txt
+#per vedere se ha funzionato basterà usare cat con top8_modifica_usrsbin.txt:
+
+cat top8_modifica_usrsbin.txt
+
+#output:
+	 ntfsclone
+	 mkntfs
+	 mount.ntfs
+	 mount.ntfs-3g
+	 ntfslabel
+	 mount.lowntfs-3g
+	 ntfscp
+	 mkfs.ntfs
