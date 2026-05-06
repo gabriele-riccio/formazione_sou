aafa0ddf0fdec2649864aeded04c70e49be7a9a4
Gabriele <ricciogabriele0@gmail.com>
2026-05-06 11:59:34 +0200

    File esercizi

---
+++ esercitazione7/shell_linux_pdf/file_esercizio1.sh
@@ -0,0 +1,14 @@
+#--Esercizio1--File in /bin che iniziano con "c"
+
+#Determinare il numero di file nella directory /bin la cui prima lettera è c.
+#prima cosa vedo cosa c'è nella directory /bin
+ls /bin
+#output:Lista di file al suo interno enorme.
+#A me interessano però quelli che iniziano con "c".
+#Eseguo:
+ls /bin/c*    #uso il glob *.
+#output: lista dei file che iniziano per c.
+
+#N.B se voglio contare anche le righe uso il pipe "|" che fa in modo che wc -l, che conta le righe, conti quelle di quello che ha in input, ovvero ls /bin/c* cioè quelle che iniziano per c.ù
+ls /bin/c* | wc -l   
+#output: nel mio caso 67
---
+++ esercitazione7/shell_linux_pdf/file_esercizio2.sh
@@ -0,0 +1,29 @@
+#--Esercizio 2--Primi 7 file di /etc in un file
+
+#Creare un file contenente i nomi dei primi 7 file della directory /etc.
+#prima cosa vedo cosa c'è nella directory /etc
+ls /etc
+#output: ModemManager     ...         ...
+         NetworkManager   ...         ...
+         Packagekit
+         UPower
+         X11
+         adduser.conf
+         alsa
+	 ...
+         ...
+#Lista di file al suo interno in ordine alfabetico, grande non enorme come prima.
+
+#Per creare il file con i primi 7 file di /etc uso la pipe | utilizzando head -7 per salvare le prime 7 righe e con la funzione freccia > li salvo in un nuovo file che crea in automatico, lo chiamo primi7.txt
+#Eseguo:
+ls /etc |  head -7 > primi7.txt
+#output: nulla, ha salvato primi7.txt
+#per vedere se ha funzionato basterà usare cat con primi7.txt
+cat primi7.txt
+#output: ModemManager
+	 NetworkManager
+	 Packagekit
+	 UPower
+	 X11
+	 adduser.conf
+	 alsa
