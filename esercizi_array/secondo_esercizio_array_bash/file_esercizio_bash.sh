f02603d2616a6272a5045c34e9168ee1a4a962d3
gabriele-riccio <gabriele.riccio@sourcesense.com>
2026-05-07 11:06:41 +0200

    feat: aggiornato file esercizio

--- esercizi_array/secondo_esercizio_array_bash/file_esercizio_bash.sh
+++ esercizi_array/secondo_esercizio_array_bash/file_esercizio_bash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#Esercizio bonus:creare un array di stringhe in 2 modi: il primo l'ho già svolto ora voglio farlo con un ciclo for per ordinarle in ordine alfabetico , togliere i duplicati e per decidere di mettere le stringhe tutte in maiuscolo o minuscolo)
+#Esercizio bonus:creare un array di stringhe in 2 modi: il primo l'ho già svolto ora voglio farlo con un ciclo for per ordinarle in ordine alfabetico, togliere i duplicati e mettere le stringhe tutte in maiuscolo o minuscolo)
 
-#
+#Seconda versione dell'esercizio, dove voglio ordinare in ordine alfabetico, rimuovere duplicati e convertire in uppercase una lista usando cicli for e array associativi (della versione >=4 di bash).
+
+INPUT=(
