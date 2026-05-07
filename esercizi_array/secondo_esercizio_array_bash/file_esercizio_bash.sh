ff0b34f55532a8bd95a28412aa3144efe83f20e0
Gabriele <ricciogabriele0@gmail.com>
2026-05-07 10:49:36 +0200

    feat: aggiunto esercizio

---
+++ esercizi_array/secondo_esercizio_array_bash/file_esercizio_bash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Esercizio bonus:creare un array di stringhe in 2 modi: il primo l'ho già svolto ora voglio farlo con un ciclo for per ordinarle in ordine alfabetico , togliere i duplicati e per decidere di mettere le stringhe tutte in maiuscolo o minuscolo)
+
+#
