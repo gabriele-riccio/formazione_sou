4e2a5f7b01c68d4c32333442dc06a013ae296e17
gabriele-riccio <gabriele.riccio@sourcesense.com>
2026-05-21 09:42:21 +0200

    refactor: Aggiunta commenti
    Added comments in Italian explaining the script's purpose and functionality.

--- esercitazione8/cleanup/file_script_1_commentato.sh
+++ esercitazione8/cleanup/file_script_1_commentato.sh
@@ -1,6 +1,11 @@
 # Cleanup
 # Run as root, of course.
 
+#Questo è uno script per svuotare i log di sistema.
+#Per prima cosa mi sposto nella directory var/log(DIRECTOY STANDARD PER I LOG DI SISTEMA)
+# Il comando cat  legge il contenuto di uno o più file e lo stampa direttamente sullo standard output, 
+#in questo caso sovrascrive quindi (sostituisce 
+
 cd /var/log
 cat /dev/null > messages
 cat /dev/null > wtmp
