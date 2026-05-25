f13a3f0172f76a2e8ce26feec5f9bb8ad6344b44
gabriele-riccio <gabriele.riccio@sourcesense.com>
2026-05-25 10:59:55 +0200

    refactor:aggiunti commenti

--- esercitazione8/cleanup/file_script_1_commentato.sh
+++ esercitazione8/cleanup/file_script_1_commentato.sh
@@ -4,7 +4,8 @@
 #Questo è uno script per svuotare i log di sistema.
 #Per prima cosa mi sposto nella directory var/log(DIRECTOY STANDARD PER I LOG DI SISTEMA)
 # Il comando cat  legge il contenuto di uno o più file e lo stampa direttamente sullo standard output, 
-#in questo caso sovrascrive quindi (sostituisce 
+#in questo caso redirige quindi su messages e wtmp il contenuto vuoto di /dev/null.
+#Alla fine stampa il messaggio di conferma
 
 cd /var/log
 cat /dev/null > messages
