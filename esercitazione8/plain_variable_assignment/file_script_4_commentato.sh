6ce944d080fa5ebb25d3c63effefb56d4d1936c5
gabriele-riccio <gabriele.riccio@sourcesense.com>
2026-05-25 12:42:59 +0200

    refactor: aggiunta commenti

--- esercitazione8/plain_variable_assignment/file_script_4_commentato.sh
+++ esercitazione8/plain_variable_assignment/file_script_4_commentato.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 # Naked variables
+#Questo script Bash serve a spiegare una regola fondamentale: il concetto di "naked variables" (variabili "nude"), 
+#ovvero quando il nome di una variabile deve essere scritto senza il simbolo $ davanti.
+
+#la regola dice che Il nome della variabile è "nudo" (senza $) quando le si sta assegnando un valore, mentre 
+#si usa il $ davanti al nome quando si vuole fare riferimento a quel valore.
+#lo script mostra questa regola attraverso 4 scenari diversi:
+
+#'Assegnazione diretta' cioè a=879 la variabile a è "nuda" perché ha inserito il valore 879 al suo interno.
+mentre quando lo stampiamo echo "The value of \"a\" is $a." usa $a perché 
+si vuole leggere e stampare a schermo il valore memorizzato (restituirà 879).
+
+#stessa cosa per le assegnazioni usando espressioni matematiche con let per fare la somma 16 + 5 a è nuda mentre 
+#quando lo stampiamo echo "The value of \"a\" is $a." usa $a perché si vuole leggere e stampare a schermo il valore memorizzato 
+#(restituirà 21).
+
+#in un ciclo for uso si effettua un'assegnazione automatica dove ad ogni iterazione del ciclo, 
+# prende un numero dall'elenco e lo assegna alla variabile a, che sarà nuda. mentre nella stampa prende il valore che ha in quel momento
+#cioè $a.
+
+#se lo scrive l'utente in input: con read ferma lo script e il terminale aspetta che l'utente scriva qualcosa
+#Quello che si scrive viene salvato nella  variabile nuda a
+#poi viene stampato come valore $a
+
+#in breve Senza $ si scrive/assegna, Con $ si legge/invoca.
+
 
 echo
 
