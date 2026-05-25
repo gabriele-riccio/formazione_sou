a23ffc87324fb2a9f92fd72be243ad6bdcebdb48
Gabriele <ricciogabriele0@gmail.com>
2026-05-25 11:22:38 +0200

    Aggiunta cartella immagini

---
+++ esercitazione8/am_i_root/file_script_2.secondo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOTUSER_NAME=root
+
+username=`id -nu`              # Or...   username=`whoami`
+if [ "$username" = "$ROOTUSER_NAME" ]
+then
+  echo "Rooty, toot, toot. You are root."
+else
+  echo "You are just a regular fella."
+fi
