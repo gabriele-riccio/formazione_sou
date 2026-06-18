8be850bf40b4703e32e0e7c4a289028018ef9642
Gabriele <ricciogabriele0@gmail.com>
2026-06-18 15:01:58 +0200

    feat: aggiunte le foto del terminale e del localhost jenkins

---
+++ esercizio_bonus_jenkins/esercizio-giorni-feriali/reset-admin.groovy
@@ -0,0 +1,8 @@
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin123")
+instance.setSecurityRealm(hudsonRealm)
+instance.save()
