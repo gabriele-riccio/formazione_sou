da1abb107a61190587d27e1a65d086939b58ff65
Gabriele <ricciogabriele0@gmail.com>
2026-06-18 16:06:04 +0200

    feat:aggiunta file di console output

--- esercizio_bonus_jenkins/esercizio-giorni-feriali/reset-admin.groovy
+++
@@ -1,8 +0,0 @@
-import jenkins.model.*
-import hudson.security.*
-
-def instance = Jenkins.getInstance()
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin123")
-instance.setSecurityRealm(hudsonRealm)
-instance.save()
